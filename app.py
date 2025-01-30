cf5a29f80dcd0179af95b8c2fd1bf89d543da138
Dekorlol <evgengolovin561@gmail.com>
2025-01-30 15:29:14 +0300

    Регистрация пользователя

---
+++ app.py
@@ -0,0 +1,155 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from contextlib import asynccontextmanager
+
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from models import User, database
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class UserData(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserCreate(UserData):
+    password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    database.connect()
+    yield
+    if not database.is_closed():
+        database.close()
+
+app = FastAPI(lifespan=lifespan)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(username: str):
+    return User.get_or_none(username=username)
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return UserData(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        disabled=user.disabled,
+    )
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return UserData(
+        username=user.username,
+        email=user.email,
+        full_name=user.full_name,
+        disabled=user.disabled,
+    )
+
+async def get_current_active_user(
+    current_user: Annotated[UserData, Depends(get_current_user)],
+):
+    print(current_user.model_dump())
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/register")
+async def register_user(username: str, password: str, full_name: str = None, email: str = None, disabled: bool = False):
+    user = User.get_or_none(username=username)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username is already in use",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    hash_password = get_password_hash(password)
+    User.create(
+        username = username,
+        full_name = full_name,
+        email = email,
+        hashed_password = hash_password,
+        disabled = disabled 
+    )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+@app.get("/users/me/", response_model=UserData)
+async def read_users_me(
+    current_user: Annotated[UserData, Depends(get_current_active_user)],
+):
+    return current_user
--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+from peewee import Model, CharField, BooleanField, SqliteDatabase
 
+database = SqliteDatabase('my_database.db')
+
+class Table(Model):
+    class Meta:
+        database = database
+
+class User(Table):
+    username = CharField(unique=True)
+    full_name = CharField(null=True)
+    email = CharField(null=True)
+    hashed_password = CharField()
+    disabled = BooleanField(default=False)
+
+def create_tables():
+    with database:
+        database.create_tables([User])
+
+create_tables()
